c2098f839a5a629cf94c8b70a31d05495cfb0d72
Rishikesh Nair <nrish123@in.ibm.com>
2018-09-21 10:38:08 +0530

    updated schema

--- models/ipams.js
+++ models/ipams.js
@@ -4,15 +4,14 @@ const Schema = mongoose.Schema;
 
 const ipamSchema = new Schema({
     network_id: String,
-    gateway: String,
     subnet_mask: String,
-    dns: String,
-    domain: String,
     ip_pool: [{
-        hostname: String,
         ipaddress: String,
         owner: String,
-        cidr: String
+        gateway: String,
+        in_use: Boolean,
+        pingable: Boolean,
+        hostname: String
     }]
 });
 
--- models/users.js
+++ models/users.js
@@ -4,15 +4,24 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     owner: String,
-    pool: [{
-        ipaddress: String,
-        network_id: String,
-        subnet_mask: String,
-        gateway: String,
-        dns: String,
-        domain: String,
-        cidr: String
-    }]
+    admin: Boolean,
+    networks: [
+        //each object is a network
+        {
+            network_id: String,
+            subnet_mask: String,
+            ip_pool: [
+                //each object is an IP in the above network
+                {
+                    ipaddress: String,
+                    gateway: String,
+                    in_use: Boolean,
+                    hostname: String,
+                    pingable: Boolean
+                }
+            ]
+        }
+    ]
 });
 
 module.exports = mongoose.model('users', userSchema);
