93f8abe558ad603b08e0af52c6e6fc66b71b4c40
Rishikesh Nair <nrish123@in.ibm.com>
2018-09-21 14:21:04 +0530

    /allocate, /assign,  allocateIP function, etc. logic changes

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,9 @@ const ipams = require('../models/ipams');
 
 const allocateIP = require('../utils/allocateIP');
 
-//////////////////GET APIs////////////////////////////
+const MAX_IPS = 10;
+
+// GET APIs
 router.get('/getUser', (req, res, next) => {
     users.findOne({
         owner: req.query.username
@@ -50,12 +52,14 @@ router.get('/getNetwork', (req, res, next) => {
 });
 
 
-//////////////////POST APIs////////////////////////////
+//POST APIs
 
+//adding new user
 router.post('/register', (req, res, next) => {
     let newUser = new users({
         owner: req.body.username,
-        pool: []
+        admin: true,
+        networks: []
     });
 
     newUser.save((err, result) => {
@@ -66,14 +70,12 @@ router.post('/register', (req, res, next) => {
     });
 });
 
+//adding new network
 router.post('/addNetwork', (req, res, next) => {
     let newNetwork = new ipams({
         network_id: req.body.network_id,
-        gateway: req.body.gateway,
         subnet_mask: req.body.subnet_mask,
-        dns: req.body.dns,
-        domain: req.body.domain,
-        cidr: req.body.cidr
+        ip_pool: []
     });
 
     newNetwork.save((err, result) => {
@@ -82,86 +84,237 @@ router.post('/addNetwork', (req, res, next) => {
             res.json(result);
         }
     });
+
 });
+
+
 
 router.post('/allocate', (req, res, next) => {
     var currentUser;
 
     users.findOne({
-        owner: req.body.username
+        owner: req.body.username,
     }, (err, result) => {
         if (err) throw err;
-        else if (!result) {
-            res.json({
-                error: "User not found"
-            });
-        } else {
-
+        else {
+            //if user exists, store his data
             currentUser = result;
+            if (!result) {
+                // null check for user, create new user if new user.
+                let newUser = new users({
+                    owner: req.body.username,
+                    admin: true,
+                    networks: []
+                });
+
+                newUser.save((err, result) => {
+                    if (err) res.json({
+                        error: err
+                    });
+                    else {
+                        console.log(result);
+                    }
+                });
+                console.log('No user found , creating new user');
+                currentUser = newUser;
+            }
+
+            
+
+            //if network is not present in users schema, then create new network with empty ip_pool
+            var networkExists = false;
+            for (network of currentUser.networks) {
+                if (network.network_id == req.body.network_id) {
+                    networkExists = true;
+                    break;
+                }
+            }
+
+            if (!networkExists) {
+                var pushNetwork = {
+                    network_id: req.body.network_id,
+                    subnet_mask: req.body.subnet_mask,
+                    ip_pool: []
+                }
+                users.updateOne({
+                    owner: currentUser.owner
+                }, {
+                    $push: {
+                        networks: pushNetwork
+                    }
+                }, (err, result) => {
+                    console.log('New network created as network not found in users');
+                });
+            }
 
             var currentNetwork;
 
             ipams.findOne({
                 network_id: req.body.network_id
             }, (err, result) => {
-                if (err) throw err;
-                else if (!result) res.json({
-                    error: "Network not found"
-                });
-                else {
-                    currentNetwork = result;
-                    var availableIPs = allocateIP(currentNetwork, req.body.cidr, req.body.count);
-                    var newIP = {
-                        hostname: currentNetwork.hostname,
-                        ipaddress: "",
-                        owner: currentUser.owner,
-                        cidr: req.body.cidr
-                    }
-                    var newUserIP = {
-                        ipaddress: "",
-                        network_id: currentNetwork.network_id,
-                        subnet_mask: currentNetwork.subnet_mask,
-                        gateway: currentNetwork.gateway,
-                        dns: currentNetwork.dns,
-                        domain: currentNetwork.domain,
-                        cidr: req.body.cidr
-                    }
+                currentNetwork = result;
 
-                    availableIPs.forEach(ip => {
-                        newIP.ipaddress = newUserIP.ipaddress = ip;
+                var IpsToAllocate = allocateIP(currentNetwork, req.body.ipRange);
 
-                        ipams.updateOne({
-                            network_id: currentNetwork.network_id
-                        }, {
-                            $push: {
-                                ip_pool: newIP
-                            }
-                        }, (err, result) => {
-                            if (err) throw err;
-                            else {
-                                console.log(currentUser.owner + ' ' + ip + 'ipams');
-                            }
-                        });
-
-                        users.updateOne({
-                            owner: currentUser.owner
-                        }, {
-                            $push: {
-                                pool: newUserIP
-                            }
-                        }, (err, result) => {
-                            if (err) throw err;
-                            else {
-                                console.log(currentUser.owner + ' ' + ip + 'users');
-                            }
-                        });
-                    });
+                var ipamIpObj = {
+                    ipaddress: "",
+                    owner: currentUser.owner,
+                    gateway: "",
+                    in_use: false,
+                    pingable: true,
+                    hostname: "rochester"
                 }
+
+                var userIpObj = {
+                    ipaddress: "",
+                    gateway: "",
+                    in_use: false,
+                    hostname: "rochester",
+                    pingable: true
+                }
+
+                console.log(IpsToAllocate);
+
+                IpsToAllocate.forEach(IpToAllocate => {
+                    ipamIpObj.ipaddress = userIpObj.ipaddress = IpToAllocate;
+
+                    ipams.updateOne({
+                        network_id: currentNetwork.network_id
+                    }, {
+                        $push: {
+                            ip_pool: ipamIpObj
+                        }
+                    }, (err, result) => {
+                        if (err) {
+                            res.json({
+                                error: "Network not found"
+                            });
+                        } else {
+
+                        }
+                    });
+
+                    users.updateOne({
+                        owner: currentUser.owner,
+                        'networks.network_id': currentNetwork.network_id
+                    }, {
+                        $push: {
+                            'networks.$.ip_pool': userIpObj
+                        }
+                    }, (err, result) => {
+                        if (err) {
+                            res.json({
+                                error: "User not found"
+                            });
+                        } else {
+                            console.log(result);
+                            console.log(IpToAllocate + '=>' + currentUser.owner);
+                        }
+                    });
+                });
+                res.json({
+                    success: IpsToAllocate
+                });
             });
         }
     });
 
 });
+
+router.post('/assign', (req, res, next) => {
+    users.findOne({
+        owner: req.body.username
+    }, (err, result) => {
+        var currentUser = result;
+        var currentNetwork = undefined;
+        var assignIps = [];
+
+        //will get the network to be worked on currently
+        for (network of currentUser.networks) {
+            if (network.network_id === req.body.network_id)
+                currentNetwork = network;
+        }
+
+
+        //will populate the assignIps array and edit the new Network object
+        for (requestedIp of req.body.arrayIP) {
+            for (pool of currentNetwork.ip_pool) {
+                if (requestedIp === pool.ipaddress) {
+                    assignIps.push(requestedIp);
+                    pool.in_use = true;
+                }
+            }
+        }
+
+        console.log(currentNetwork);
+
+
+        assignIps.forEach(ip => {
+            //update the IPAMS collection
+            ipams.updateOne({
+                'ip_pool.ipaddress': ip
+            }, {
+                $set: {
+                    'ip_pool.$.in_use': true
+                }
+            }, (err, result) => {
+                if (err) res.json({
+                    error: err
+                });
+                else if (!result) res.json({
+                    error: 'No such IP found'
+                });
+                else {
+                    console.log('ipams collection updated -> ' + result);
+                }
+            });
+
+        });
+
+        //pull old network
+        users.updateOne({
+            owner: currentUser.owner
+        }, {
+            $pull: {
+                networks: {
+                    network_id: currentNetwork.network_id
+                }
+            }
+        }, (err, result) => {
+            if (err) res.json({
+                error: err
+            });
+            else if (!result) res.json({
+                error: 'No such user found'
+            });
+            else {
+
+                //push new network
+                users.updateOne({
+                    owner: currentUser.owner
+                }, {
+                    $push: {
+                        networks: currentNetwork
+                    }
+                }, (err, result) => {
+                    if (err) res.json({
+                        error: err
+                    });
+                    else if (!result) res.json({
+                        error: 'No such user found'
+                    });
+                    else {
+                        console.log(result);
+                    }
+                });
+            }
+        });
+
+
+
+    });
+});
+
 
 router.post('/free', (req, res, next) => {
     var currentUser;
@@ -214,3 +367,7 @@ router.post('/free', (req, res, next) => {
 });
 
 module.exports = router;
+
+
+//ping
+//ns-lookup
--- utils/allocateIP.js
+++ utils/allocateIP.js
@@ -1,31 +1,23 @@
-const Netmask = require('netmask').Netmask;
-
-function allocateIP(currentNetwork, cidr, count) { 
-    const block = new Netmask(currentNetwork.network_id + '/' + cidr);
-    var netmaskArray = [];
-    var currentNetworkPool = [];
+function allocateIP(currentNetwork, ipRange) {
+    let lowerBound = parseInt(ipRange[0].split('.')[3]); //take the last part of IP and convert to integer
+    let upperBound = parseInt(ipRange[1].split('.')[3]); //take the last part of IP and convert to integer
 
     var allocatedIPs = [];
 
-    //existing ips in current network
-    currentNetwork.ip_pool.forEach(obj => {
-        currentNetworkPool.push(obj.ipaddress);
-    });
-    //all ips in the range 
-    block.forEach(ip => {
-        netmaskArray.push(ip);
-    });
+    for (var i = lowerBound; i < upperBound; i++) {
+        var ipFound = false;
+        var checkIP = (ipRange[0].split('.')[0] + '.' + ipRange[0].split('.')[1] + '.' + ipRange[0].split('.')[2] + '.' + i.toString());
+        currentNetwork.ip_pool.forEach(ip => {
+            if (ip.ipaddress === checkIP) {
+                ipFound = true;
+            }
+        });
 
-    for (ip of netmaskArray) {
-        if (!currentNetworkPool.includes(ip)) {
-            if (allocatedIPs.length < count)
-                allocatedIPs.push(ip);
-            else
-                break;
+        if (!ipFound) {
+            allocatedIPs.push(checkIP);
         }
     }
-    
-    return allocatedIPs;   
+    return allocatedIPs;
 }
 
 module.exports = allocateIP;
