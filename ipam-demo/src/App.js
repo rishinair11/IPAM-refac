5d6a8f3bb81c8b9e7ba8ca27420c5b59416a3d3e
Rishikesh Nair <nrish123@in.ibm.com>
2018-10-07 12:43:11 +0530

    react-md integration

--- ipam-demo/src/App.js
+++ ipam-demo/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
+import { NavigationDrawer } from 'react-md';
 import logo from './logo.svg';
 import './App.css';
+
+import WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md"
+      >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </div>
+      </NavigationDrawer>
     );
   }
 }
